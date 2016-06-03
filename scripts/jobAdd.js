fa6b4393fabe48a054659d210247713e934a0d44
jisenber <isenberg.jacob@yahoo.com>
2016-06-02 23:10:50 -0700

    Edits to assignment1 to make to suitable for resubmission

--- scripts/jobAdd.js
+++ scripts/jobAdd.js
@@ -12,11 +12,11 @@ function employment (job) {
 employment.prototype.toHtml = function() {
   var $newJob = $('div.template').clone();
 
-  $newJob.find('.position').html('Position: ' + this.position);
-  $newJob.find('.institution').html('Institution: ' + this.institution);
-  $newJob.find('.boss').html('Boss: ' + this.boss);
-  $newJob.find('.website').html('Website: ' + this.website);
-  $newJob.find('.role').html('Role: ' + this.role);
+  $newJob.find('.position').html('<b>Position:</b> ' + this.position);
+  $newJob.find('.institution').html('<b>Institution:</b> ' + this.institution);
+  $newJob.find('.boss').html('<b>Boss:</b> ' + this.boss);
+  $newJob.find('.website').html('<b>Website:</b> ' + this.website);
+  $newJob.find('.role').html('<b>Role:</b> ' + this.role);
 
   $newJob.append('<hr>');
 
@@ -29,5 +29,5 @@ jobData.forEach(function(ele) {
 });
 
 employments.forEach(function(a){
-  $('#jobList').append(a.toHtml())
+  $('#jobList').append(a.toHtml());
 });
